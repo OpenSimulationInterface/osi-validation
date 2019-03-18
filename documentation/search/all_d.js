ae5d566cb92083c9a023db5fac7073b3157299ec
Aina Rasoldier <aina.rasoldier@insa-lyon.fr>
2019-03-18 18:24:11 +0100

    Add Sensor Data KPIs definition and update the deployment

--- documentation/search/all_d.js
+++ documentation/search/all_d.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['seat',['Seat',['../class_occupant_1_1_classification_1_1_seat.html',1,'Occupant::Classification']]],
+  ['sensordata',['SensorData',['../class_sensor_data.html',1,'']]],
   ['sensorview',['SensorView',['../class_sensor_view.html',1,'']]],
   ['sensorviewconfiguration',['SensorViewConfiguration',['../class_sensor_view_configuration.html',1,'']]],
   ['spherical3d',['Spherical3d',['../class_spherical3d.html',1,'']]],
--- documentation/search/all_e.js
+++ documentation/search/all_e.js
@@ -5,5 +5,5 @@ var searchData=
   ['trafficlight',['TrafficLight',['../class_traffic_light.html',1,'']]],
   ['trafficsign',['TrafficSign',['../class_traffic_sign.html',1,'']]],
   ['trafficsignvalue',['TrafficSignValue',['../class_traffic_sign_value.html',1,'']]],
-  ['type',['Type',['../class_road_marking_1_1_classification_1_1_type.html',1,'RoadMarking::Classification::Type'],['../class_traffic_sign_1_1_supplementary_sign_1_1_classification_1_1_type.html',1,'TrafficSign::SupplementarySign::Classification::Type'],['../class_lane_boundary_1_1_classification_1_1_type.html',1,'LaneBoundary::Classification::Type'],['../class_lane_1_1_classification_1_1_type.html',1,'Lane::Classification::Type'],['../class_traffic_sign_1_1_main_sign_1_1_classification_1_1_type.html',1,'TrafficSign::MainSign::Classification::Type']]]
+  ['type',['Type',['../class_lane_boundary_1_1_classification_1_1_type.html',1,'LaneBoundary::Classification::Type'],['../class_traffic_sign_1_1_main_sign_1_1_classification_1_1_type.html',1,'TrafficSign::MainSign::Classification::Type'],['../class_lane_1_1_classification_1_1_type.html',1,'Lane::Classification::Type'],['../class_traffic_sign_1_1_supplementary_sign_1_1_classification_1_1_type.html',1,'TrafficSign::SupplementarySign::Classification::Type'],['../class_road_marking_1_1_classification_1_1_type.html',1,'RoadMarking::Classification::Type']]]
 ];
