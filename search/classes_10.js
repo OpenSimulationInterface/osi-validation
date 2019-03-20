319ea3c7bae7b3ab2a3dfdef9966b6feb19161fb
Aina Rasoldier <aina.rasoldier@insa-lyon.fr>
2019-03-20 13:00:56 +0100

    Cleaning the github page root

--- search/classes_10.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['variability',['Variability',['../class_traffic_sign_1_1_variability.html',1,'TrafficSign']]],
-  ['vector2d',['Vector2d',['../class_vector2d.html',1,'']]],
-  ['vector3d',['Vector3d',['../class_vector3d.html',1,'']]]
-];
--- search/classes_a.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['occupant',['Occupant',['../class_occupant.html',1,'']]],
-  ['orientation3d',['Orientation3d',['../class_orientation3d.html',1,'']]]
-];
