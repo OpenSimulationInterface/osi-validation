519d4174ea9dcf6b5bffc2f18fa56714d29f14cb
Aina Rasoldier <aina.rasoldier@insa-lyon.fr>
2019-04-17 11:07:52 +0200

    Add KPIs to documentation (architecture change of GH pages)

---
+++ docs/KPIs/search/classes_2.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['camerasensorview',['CameraSensorView',['../class_camera_sensor_view.html',1,'']]],
+  ['camerasensorviewconfiguration',['CameraSensorViewConfiguration',['../class_camera_sensor_view_configuration.html',1,'']]],
+  ['channelformat',['ChannelFormat',['../class_camera_sensor_view_configuration_1_1_channel_format.html',1,'CameraSensorViewConfiguration']]],
+  ['classification',['Classification',['../class_traffic_sign_1_1_supplementary_sign_1_1_classification.html',1,'TrafficSign::SupplementarySign::Classification'],['../class_lane_boundary_1_1_classification.html',1,'LaneBoundary::Classification'],['../class_occupant_1_1_classification.html',1,'Occupant::Classification'],['../class_lane_1_1_classification.html',1,'Lane::Classification'],['../class_traffic_light_1_1_classification.html',1,'TrafficLight::Classification'],['../class_road_marking_1_1_classification.html',1,'RoadMarking::Classification'],['../class_traffic_sign_1_1_main_sign_1_1_classification.html',1,'TrafficSign::MainSign::Classification']]],
+  ['color',['Color',['../class_lane_boundary_1_1_classification_1_1_color.html',1,'LaneBoundary::Classification::Color'],['../class_traffic_light_1_1_classification_1_1_color.html',1,'TrafficLight::Classification::Color'],['../class_road_marking_1_1_classification_1_1_color.html',1,'RoadMarking::Classification::Color']]]
+];
--- docs/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['osi_2ddata_2dvalidation',['osi-data-validation',['../index.html',1,'']]]
-];
