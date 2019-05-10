3676385b9d3f6e6db449ee624ae7304a5ea0197a
Aina Rasoldier <aina.rasoldier@insa-lyon.fr>
2019-05-10 18:12:32 +0200

    update on the generation of documentation from yml

--- generic-validator/osi_rules_documentation.py
+++ generic-validator/osi_rules_documentation.py
@@ -1,6 +1,7 @@
 from xml.dom import minidom
 
-from osi_validation_rules import OSIValidationRules, MessageType, Field
+from osi_validation_rules import OSIValidationRules, MessageType
+
 
 class OSIRulesDocumentation:
     """This class creates a readable documentation from a set of rules. It is
@@ -10,40 +11,38 @@ class OSIRulesDocumentation:
     def __init__(self, ovr):
         self.rules = ovr.t_rules
         self.document = minidom.Document()
+        self.html_list = self.document.createElement('ul')
+        self.html_list.setAttribute('id', 'myUL')
 
     def create_documentation(self):
         """Create the documentation
         """
-        self.html_list = self.document.createElement('ul')
         self.scan_node(self.rules, 0)
         self.document.appendChild(self.html_list)
 
     def scan_node(self, node, deepness):
         """Scan recursively a node with a DFS style.
         """
-        for key, value in node.nested_types.items():
+        for _key, value in node.nested_types.items():
             self.scan_node(value, deepness + 1)
 
         if isinstance(node, MessageType):
-            for field, field_obj in node.fields.items():
+            for _field, field_obj in node.fields.items():
                 self.scan_field(field_obj)
-
 
     def scan_field(self, field):
         """Scan a list of rules
         """
-        # for rule in field.rules:
-        #     print(rule)
-        print(field.message_path, field)
         list_element = self.document.createElement('li')
         link = self.document.createElement('a')
         link.setAttribute('href', '#')
-        print(field.message_path.pretty_html())
-        text = minidom.parseString('<span>' + field.message_path.pretty_html() + '</span>')._get_firstChild()
+        text = minidom.parseString(
+            '<span>' + field.message_path.pretty_html() + '</span>').firstChild
         link.appendChild(text)
 
         list_element.appendChild(link)
         self.html_list.appendChild(list_element)
+
 
 if __name__ == "__main__":
     OVR = OSIValidationRules()
