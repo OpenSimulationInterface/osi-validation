d09871d8f277c87e2ee84d590b3bf0d6055bbfdc
Aina Rasoldier <aina.rasoldier@insa-lyon.fr>
2019-05-17 12:05:36 +0200

    Move documentation to cleaner tree

---
+++ KPIs/search/all_11.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['variability',['Variability',['../class_traffic_sign_1_1_variability.html',1,'TrafficSign']]],
+  ['vector2d',['Vector2d',['../class_vector2d.html',1,'']]],
+  ['vector3d',['Vector3d',['../class_vector3d.html',1,'']]]
+];
---
+++ _sources/readme_link.md.txt
@@ -0,0 +1,4 @@
+Installation
+============
+
+.. include:: ../README.md
